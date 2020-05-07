3ba86ab8983aaf068fbe3adf6c8cce4a828f5b5a
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-05-07 08:51:56 +0200

    Update comandi.py

--- comandi/comandi.py
+++ comandi/comandi.py
@@ -17,7 +17,7 @@ class ServStatus(commands.Cog):
       @commands.command(aliases=["stato"])
       async def statomc(self, ctx):
             """Mostra lo stato del server Minecraft."""
-            response = await self.bot.session.get("http://vps.vincysuper07.cf/vincystatus/api.php")
+            response = await self.bot.session.get("http://statomc.vincysuper07.cf/api.php")
             status = (await response.content.readline()).decode('UTF-8')
             embed = discord.Embed(title = "Server Minecraft: mc.Vincysuper07.cf", description = f"Al momento il server è {status}")
             if status == "OFFLINE.":
